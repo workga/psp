1903e02561009f1741d511a198bcd67b2202b9b1
Gleb Andreevich <work.ga@yandex.ru>
2024-01-07 23:24:43 +0300

    add product details modal

---
+++ frontend/src/components/pages/Search/ProductModal.js
@@ -0,0 +1,59 @@
+import "./ProductModal.css"
+import "./Search.css"
+
+function ProductDetails({selectedProduct}) {
+	if (selectedProduct == null) {
+		return null
+	}
+	return (
+		<div className="product-details">
+			<div className="flex-row">
+				<img className="product-image"
+					src={selectedProduct.image || 'product_photo_placeholder.png'}
+				/>
+				<div className="search-result-info">
+					<div className="product-price">{selectedProduct.price}</div>
+					<div className="product-description">
+						Brand: {selectedProduct.car_info.brand_name}
+						<br/>
+						Model: {selectedProduct.car_info.model_name}
+						<br/>
+						Generation: {selectedProduct.car_info.gen_name}
+						<br/>
+						Category: {selectedProduct.detail_info.category_name}
+						<br/>
+						Type: {selectedProduct.detail_info.type_name}
+						<br/>
+						<br/>
+						{selectedProduct.description}
+					</div>
+				</div>
+			</div>
+			<div className="product-details-bottom">
+				<div className="mark product-details-mark">{selectedProduct.condition === "new" ? "Новая" : "Б/у"}</div>
+				<div className="mark product-details-mark">{selectedProduct.address}</div>
+				<div className="mark product-details-mark">{selectedProduct.phone}</div>
+			</div>
+		</div>
+	)
+}
+
+export function ProductModal({active, setActive, selectedProduct, setSelectedProduct}) {
+	function activeModifier(className) {
+		if (active) {
+			className += " active"
+		}
+		return className
+	}
+
+	return (
+		<div className={activeModifier("product-modal")} onClick={() => {
+			setSelectedProduct(null);
+			setActive(false);
+		}}>
+			<div className={activeModifier("product-modal-content")} onClick={(e) => {e.stopPropagation()}}>
+				<ProductDetails selectedProduct={selectedProduct}/>
+			</div>
+		</div>
+	);
+}
--- frontend/src/components/pages/Search/Search.js
+++ frontend/src/components/pages/Search/Search.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocalStorage } from "@uidotdev/usehooks";
 import axios from 'axios';
 import './Search.css';
+import { ProductModal } from "./ProductModal.js"
 
 export function Search() {
   const [brands, setBrands] = useState([]);
@@ -24,6 +25,9 @@ export function Search() {
 
 
   const [searchResults, setSearchResults] = useState([]);
+
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [productModalActive, setProductModalActive] = useState(false);
 
   useEffect(() => {
     async function fetchBrands() {
@@ -176,89 +180,98 @@ export function Search() {
   };
 
   return (
-		<div className="search-container">
-			<div className="search-header">
-				<form className="search-form" onSubmit={handleSubmit}>
-					<div className="search-form-row">
-						<input className="search-input" type="text" placeholder="Марка автомобиля" value={brandSearch} onChange={handleBrandInputChange} list="brandsList" />
-						<datalist className="search-input" id="brandsList">
-							{brands.map((brand) => (
-								<option key={brand.id} value={brand.brand_name}>
-									{brand.brand_name}
-								</option>
-							))}
-						</datalist>
-					</div>
-
-					<div className="search-form-row">
-						<input className="search-input" type="text" placeholder="Модель автомобиля" value={modelSearch} onChange={handleModelInputChange} list="modelsList" />
-						<datalist className="search-input" id="modelsList">
-							{models.map((model) => (
-								<option key={model.id} value={model.model_name}>
-									{model.model_name}
-								</option>
-							))}
-						</datalist>
-					</div>
-
-					<div className="search-form-row">
-						<input className="search-input" type="text" placeholder="Поколение автомобиля" value={genSearch} onChange={handleGenInputChange} list="gensList" />
-						<datalist className="search-input" id="gensList">
-							{gens.map((gen) => (
-								<option key={gen.id} value={gen.gen_name}>
-									{gen.gen_name}
-								</option>
-							))}
-						</datalist>
-					</div>
-
-					<div className="search-form-row">
-						<input className="search-input" type="text" placeholder="Категория детали" value={categorySearch} onChange={handleCategoryInputChange} list="categoriesList" />
-						<datalist className="search-input" id="categoriesList">
-							{categories.map((category) => (
-								<option key={category.id} value={category.category_name}>
-									{category.category_name}
-								</option>
-							))}
-						</datalist>
-					</div>
-
-					<div className="search-form-row">
-						<input className="search-input" type="text" placeholder="Тип детали" value={typeSearch} onChange={handleTypeInputChange} list="typesList" />
-						<datalist className="search-input" id="typesList">
-							{types.map((type) => (
-								<option key={type.id} value={type.type_name}>
-									{type.type_name}
-								</option>
-							))}
-						</datalist>
-					</div>
-
-					<button className="search-button" type="submit">Искать</button>
-      	</form>
-			</div>
-
-
-			<div className="search-results-container">
-				{searchResults.map((result) => (
-					<div className="search-result-item">
-						<div className="flex-row">
-							<img className="search-result-image"
-								src={result.image || 'product_photo_placeholder.png'}
-								alt="Фотография продукта"
-							/>
-							<div className="search-result-info">
-								<div className="search-result-item-price">{result.price}</div>
-								<div className="search-result-item-description">{result.description.slice(0, 480) + (result.description.length > 480 ? "..." : "")}</div>
+  	<div>
+  		<main>
+				<div className="search-container">
+					<div className="search-header">
+						<form className="search-form" onSubmit={handleSubmit}>
+							<div className="search-form-row">
+								<input className="search-input" type="text" placeholder="Марка автомобиля" value={brandSearch} onChange={handleBrandInputChange} list="brandsList" />
+								<datalist className="search-input" id="brandsList">
+									{brands.map((brand) => (
+										<option key={brand.id} value={brand.brand_name}>
+											{brand.brand_name}
+										</option>
+									))}
+								</datalist>
 							</div>
-						</div>
-						<div className="flex-row search-result-item-bottom">
-							<div className="search-result-item-condition">{result.condition === "new" ? "Новая" : "Б/у"}</div>
-							<button className="search-result-item-details-button">Подробнее</button>
-						</div>
+
+							<div className="search-form-row">
+								<input className="search-input" type="text" placeholder="Модель автомобиля" value={modelSearch} onChange={handleModelInputChange} list="modelsList" />
+								<datalist className="search-input" id="modelsList">
+									{models.map((model) => (
+										<option key={model.id} value={model.model_name}>
+											{model.model_name}
+										</option>
+									))}
+								</datalist>
+							</div>
+
+							<div className="search-form-row">
+								<input className="search-input" type="text" placeholder="Поколение автомобиля" value={genSearch} onChange={handleGenInputChange} list="gensList" />
+								<datalist className="search-input" id="gensList">
+									{gens.map((gen) => (
+										<option key={gen.id} value={gen.gen_name}>
+											{gen.gen_name}
+										</option>
+									))}
+								</datalist>
+							</div>
+
+							<div className="search-form-row">
+								<input className="search-input" type="text" placeholder="Категория детали" value={categorySearch} onChange={handleCategoryInputChange} list="categoriesList" />
+								<datalist className="search-input" id="categoriesList">
+									{categories.map((category) => (
+										<option key={category.id} value={category.category_name}>
+											{category.category_name}
+										</option>
+									))}
+								</datalist>
+							</div>
+
+							<div className="search-form-row">
+								<input className="search-input" type="text" placeholder="Тип детали" value={typeSearch} onChange={handleTypeInputChange} list="typesList" />
+								<datalist className="search-input" id="typesList">
+									{types.map((type) => (
+										<option key={type.id} value={type.type_name}>
+											{type.type_name}
+										</option>
+									))}
+								</datalist>
+							</div>
+
+							<button className="search-button" type="submit">Искать</button>
+						</form>
 					</div>
-				))}
-			</div>
+
+
+					<div className="search-results-container">
+						{searchResults.map((result) => (
+							<div className="search-result-item">
+								<div className="flex-row">
+									<img className="search-result-image"
+										src={result.image || 'product_photo_placeholder.png'}
+									/>
+									<div className="search-result-info">
+										<div className="search-result-item-price">{result.price}</div>
+										<div className="search-result-item-description">{result.description.slice(0, 480) + (result.description.length > 480 ? "..." : "")}</div>
+									</div>
+								</div>
+								<div className="flex-row flex-space-between">
+									<div className="mark">{result.condition === "new" ? "Новая" : "Б/у"}</div>
+									<button className="search-result-item-details-button" onClick={() => {
+										setSelectedProduct(result);
+  									setProductModalActive(true);
+									}}>
+										Подробнее
+									</button>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</main>
+			<ProductModal active={productModalActive} setActive={setProductModalActive} selectedProduct={selectedProduct} setSelectedProduct={setSelectedProduct}/>
     </div>
   );
 }
